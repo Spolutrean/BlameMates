28b330fac1f489b2e81820689d2c0be07d500e46
Andrew Kashukov <andrei1999kashukov@gmail.com>
2020-06-11 22:06:42 +0300

    Implement UserInformationService (draft version, not tested yet)

--- src/main/java/com/blame/mates/UserInformationService.java
+++ src/main/java/com/blame/mates/UserInformationService.java
@@ -1,11 +1,19 @@
 package com.blame.mates;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.intellij.openapi.components.Service;
 import com.intellij.openapi.components.ServiceManager;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.*;
 
 /**
  * A service for interacting with user information.
@@ -13,12 +21,40 @@ import java.util.List;
 @Service
 public final class UserInformationService {
 
+    private Map<String, List<ContactMethod>> userInformation;
+
     public UserInformationService() {
     }
 
     @NotNull
     public List<ContactMethod> getUserContactMethods(String userEmail) {
-        return new ArrayList<>();
+        if (userInformation == null) {
+            String userInformationJson = "";
+            try {
+                List<String> jsonLines = Files.readAllLines(Paths.get("blameMatesData.json"));
+                userInformationJson = String.join("", jsonLines);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                userInformation = objectMapper.
+                        readValue(userInformationJson, new TypeReference<>() {
+                        });
+            } catch (JsonParseException | JsonMappingException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        List<ContactMethod> userContactMethods = userInformation.get(userEmail);
+        if (userContactMethods != null) {
+            return userContactMethods;
+        } else {
+            return new ArrayList<>();
+        }
     }
 
     public static UserInformationService getInstance() {

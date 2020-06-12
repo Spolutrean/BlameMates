83526141748011f0b0d41519f2a0f66176e07d8d
Andrew Kashukov <andrei1999kashukov@gmail.com>
2020-06-12 21:45:07 +0300

    Reorganize UserInformationService to isolate repetitive behavior; Implement addition of contact methods

--- src/main/java/com/blame/mates/UserInformationService.java
+++ src/main/java/com/blame/mates/UserInformationService.java
@@ -6,12 +6,11 @@ import com.intellij.openapi.components.Service;
 import com.intellij.openapi.components.ServiceManager;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import com.fasterxml.jackson.databind.*;
 
@@ -21,33 +20,79 @@ import com.fasterxml.jackson.databind.*;
 @Service
 public final class UserInformationService {
 
+    private static final String PATH_TO_DATA = "blameMatesData.json";
     private Map<String, List<ContactMethod>> userInformation;
 
     public UserInformationService() {
     }
 
-    @NotNull
-    public List<ContactMethod> getUserContactMethods(String userEmail) {
-        if (userInformation == null) {
-            String userInformationJson = "";
-            try {
-                List<String> jsonLines = Files.readAllLines(Paths.get("blameMatesData.json"));
-                userInformationJson = String.join("", jsonLines);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    private void readUserInformation() {
+        //TODO: implement proper exception handling with popups
 
-            ObjectMapper objectMapper = new ObjectMapper();
+        File userInformationFile = new File(PATH_TO_DATA);
+        if (!userInformationFile.isFile()) {
             try {
-                userInformation = objectMapper.
-                        readValue(userInformationJson, new TypeReference<>() {
-                        });
-            } catch (JsonParseException | JsonMappingException e) {
-                e.printStackTrace();
+                userInformationFile.createNewFile();
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+
+        String userInformationJson = "";
+        try {
+            userInformationJson = String.join("", Files.readAllLines(userInformationFile.toPath()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            userInformation = new ObjectMapper().
+                    readValue(userInformationJson, new TypeReference<Map<String, List<ContactMethod>>>() {
+                    });
+        } catch (JsonParseException | JsonMappingException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (userInformation == null) {
+            userInformation = new HashMap<String, List<ContactMethod>>();
+        }
+    }
+
+    private void writeUserInformation() {
+        //TODO: implement proper exception handling with popups
+
+        File userInformationFile = new File(PATH_TO_DATA);
+        /*
+        if (!userInformationFile.isFile()) {
+            try {
+                userInformationFile.createNewFile();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        */
+
+        String userInformationJson = "";
+        try {
+            userInformationJson = new ObjectMapper().writeValueAsString(userInformation);
+        } catch (JsonParseException | JsonMappingException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try (FileWriter fileWriter = new FileWriter(userInformationFile, false)) {
+            fileWriter.write(userInformationJson);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @NotNull
+    public List<ContactMethod> getUserContactMethods(String userEmail) {
+        readUserInformation();
 
         List<ContactMethod> userContactMethods = userInformation.get(userEmail);
         if (userContactMethods != null) {
@@ -55,6 +100,25 @@ public final class UserInformationService {
         } else {
             return new ArrayList<>();
         }
+    }
+
+    public void addUserContactMethod(String userEmail, ContactMethod contactMethod) {
+        if (contactMethod == null) {
+            return;
+        }
+
+        readUserInformation();
+        if (userInformation.get(userEmail) == null) {
+            userInformation.put(userEmail, Arrays.asList(contactMethod));
+        }
+        else {
+            userInformation.get(userEmail).add(contactMethod);
+        }
+        writeUserInformation();
+    }
+
+    public void removeUserContactMethod(String userEmail, ContactMethod contactMethod) {
+        //TODO: implement contact method removal; think about user removal implementation
     }
 
     public static UserInformationService getInstance() {
